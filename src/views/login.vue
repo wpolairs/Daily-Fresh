2bad5ccf98a538ec9f3def44ad1b0913e06b6505
王 剑 <wpolairs@gmail.com>
2021-02-05 17:18:26 +0800

    登录界面实现

---
+++ src/views/login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login">
+      <a-form
+        id="components-form-demo-normal-login"
+        :form="form"
+        class="login-form"
+        @submit="handleSubmit"
+      >
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'userName',
+              {
+                rules: [
+                  { required: true, message: '请输入邮箱！' },
+                ],
+              },
+            ]"
+            placeholder="Email"
+          >
+            <a-icon
+              slot="prefix"
+              type="user"
+              style="color: rgba(0, 0, 0, 0.25)"
+            />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              {
+                rules: [
+                  { required: true, message: '请输入密码！' },
+                ],
+              },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"
+            />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-checkbox
+            v-decorator="[
+              'remember',
+              {
+                valuePropName: 'checked',
+                initialValue: true,
+              },
+            ]"
+          >
+            记住密码
+          </a-checkbox>
+          <a class="login-form-forgot" href=""> 忘记密码 </a>
+          <a-button type="primary" html-type="submit" class="login-form-button">
+            登录
+          </a-button>
+          <a href=""> 现在注册 </a>
+        </a-form-item>
+      </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+  },
+};
+</script>
+<style>
+@import url('~@/assets/css/login.less');
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 100%;
+}
+</style>
